20c155085f8082fe4b31b0655c7d7f9b3569f79a
skaragher <skaragher@gmail.com>
2026-03-27 14:32:16 +0000

    fix: allow HTTP on Android/iOS mobile and fix fallback API URL to http

--- mobile-driver/src/config.js
+++ mobile-driver/src/config.js
@@ -3,4 +3,4 @@ import Constants from "expo-constants";
 export const API_URL =
   Constants.expoConfig?.extra?.apiUrl ||
   Constants.manifest2?.extra?.expoClient?.extra?.apiUrl ||
-  "https://193.168.173.181/api";
+  "http://193.168.173.181/api";
